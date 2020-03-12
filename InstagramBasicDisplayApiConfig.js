789ea7036e43fb8d69af1884b76ac0358e881a4b
Chris Thomson <7858587+chriswthomson@users.noreply.github.com>
2020-03-12 14:21:19 +0000

    Update InstagramBasicDisplayApiConfig.js

--- InstagramBasicDisplayApiConfig.js
+++ InstagramBasicDisplayApiConfig.js
@@ -9,94 +9,40 @@
 var InstagramBasicDisplayApiConfig = {
 
 	init: function() {
-		this.copyAuthUri();
-		this.toggleClientSecret();
+		this.iconFix();
+		this.trash();
 	},
 
-	iconWidth: 40,
+	iconFix: function() { // For PW 2.7.2
 
-	copyAuthUri: function() {
+		var $spans = $("input.remove-account + span");
+		if(!$spans.length) return;
 
-		var $inputs = $("[data-copy]");
+		$spans.each(function() {
+			var $span = $(this);
+			if(!$span.children("i").length) {
+				$span.html("<i class='fa fa-trash'></i>");
+			}
+		});
+	},
+
+	trash: function() {
+
+		var $inputs = $("input.remove-account");
 		if(!$inputs.length) return;
 
-		var this$1 = this;
-		$inputs.each(function() {
-
+		$inputs.on("click", function() {
 			var $input = $(this);
-			var $data = $input.data("copy")
-			var id = "copy-" + $input.attr("id");
-			this$1.setInputWidth($input);
-			$input.after(this$1.renderButton(this$1.renderIcon("files-o", $data.off), id));
-
-			$("#" + id).on("click", function(e) {
-
-				e.preventDefault();
-
-				$input.select();
-				document.execCommand("copy");
-				
-				var $button = $(this);
-				var lbl = $button.html();
-
-				$button.html(this$1.renderIcon("check", $data.on));
-				$button.attr("data-uk-tooltip", $data.on);
-				setTimeout(function() {
-					$button.html(lbl);
-					$button.removeAttr("data-uk-tooltip");
-				}, 2048);
-			});
-		});
-	},	
-
-	renderButton: function(label, id, cls) {
-		if(cls === void 0) cls = "";
-		return "<a " + [
-			"href='#'",
-			"id='" + id + "'",
-			"class='" + cls + "'",
-			"style='display:inline-block;width:" + this.iconWidth + "px;text-align:center;'"
-		].join(" ") + ">" + label + "</a>";
-	},
-
-	renderIcon: function(icon, title) {
-		return "<i " + [
-			"class='fa fa-" + icon + "'",
-			"title='" + title + "'",
-			"aria-hidden='true'",
-		].join(" ") + "></i>";
-	},
-
-	setInputWidth: function($input) {
-		$input.css("width", "calc(100% - " + this.iconWidth + "px)");
-	},
-
-	toggleClientSecret: function() {
-
-		var $input = $("#Inputfield_clientSecret");
-		if(!$input.length) return;
-
-		var id = "toggle-client-secret";
-		var idShow = id + "-show";
-		var lbl = [this.renderIcon("eye", "Show"), this.renderIcon("eye-slash", "Hide")]
-
-		this.setInputWidth($input);
-		$input.after(this.renderButton(lbl[0], id, idShow));
-
-		$("#" + id).on("click", function(e) {
-
-			e.preventDefault();
-
-			var $button = $(this);
-			if($button.hasClass(idShow)) {
-				$input[0].type = "text";
-				$button.html(lbl[1]);
+			var $row = $input.closest("tr");
+			if($input.is(":checked")) {
+				if(confirm("Are you sure you want to remove " + $input.val() + "?\nThe account will be removed once you click Submit.")) {
+					$row.addClass("removing-account");
+				} else {
+					$input.attr("checked", false);
+				}
 			} else {
-				$input[0].type = "password";
-				$button.html(lbl[0]);
+				$row.removeClass("removing-account");
 			}
-			
-			$button.toggleClass(idShow + " " + id + "-hide");
 		});
 	}
 };
