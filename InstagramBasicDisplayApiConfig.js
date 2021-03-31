4ea2a111232da01bde9946035884ca4d9b337192
Chris Thomson <7858587+chriswthomson@users.noreply.github.com>
2021-03-31 11:09:02 +0100

    Update InstagramBasicDisplayApiConfig.js
    Year update, apostrophe OCD

--- InstagramBasicDisplayApiConfig.js
+++ InstagramBasicDisplayApiConfig.js
@@ -1,7 +1,7 @@
 /**
  * Instagram Basic Display API Config JS
  *
- * @copyright 2020 NB Communication Ltd
+ * @copyright 2021 NB Communication Ltd
  * @license Mozilla Public License v2.0 http://mozilla.org/MPL/2.0/
  *
  */
@@ -15,33 +15,33 @@ var InstagramBasicDisplayApiConfig = {
 
 	iconFix: function() { // For PW 2.7.2
 
-		var $spans = $("input.remove-account + span");
+		var $spans = $('input.remove-account + span');
 		if(!$spans.length) return;
 
 		$spans.each(function() {
 			var $span = $(this);
-			if(!$span.children("i").length) {
-				$span.html("<i class='fa fa-trash'></i>");
+			if(!$span.children('i').length) {
+				$span.html('<i class="fa fa-trash"></i>');
 			}
 		});
 	},
 
 	trash: function() {
 
-		var $inputs = $("input.remove-account");
+		var $inputs = $('input.remove-account');
 		if(!$inputs.length) return;
 
-		$inputs.on("click", function() {
+		$inputs.on('click', function() {
 			var $input = $(this);
-			var $row = $input.closest("tr");
-			if($input.is(":checked")) {
-				if(confirm("Are you sure you want to remove " + $input.val() + "?\nThe account will be removed once you click Submit.")) {
-					$row.addClass("removing-account");
+			var $row = $input.closest('tr');
+			if($input.is(':checked')) {
+				if(confirm('Are you sure you want to remove ' + $input.val() + "?\nThe account will be removed once you click Submit.")) {
+					$row.addClass('removing-account');
 				} else {
-					$input.attr("checked", false);
+					$input.attr('checked', false);
 				}
 			} else {
-				$row.removeClass("removing-account");
+				$row.removeClass('removing-account');
 			}
 		});
 	}
